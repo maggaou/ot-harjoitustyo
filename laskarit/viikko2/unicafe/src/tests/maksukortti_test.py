33a26ff1e42b53b8fbb7316a413ea95cdefef0f5
Oula Magga <oula.magga@helsinki.fi>
2025-10-28 12:40:09 +0200

    Viikko 2, tehtävä 7. Testit/kortin alkusaldo on oikein

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,6 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_alkusaldo_on_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
