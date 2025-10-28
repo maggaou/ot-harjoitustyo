e5dea1ddf9bd4fda09abf77391f87ecdf08ee46c
Oula Magga <oula.magga@helsinki.fi>
2025-10-28 13:09:20 +0200

    Viikko 2, tehtävä 7. Testit/ota_rahaa palauttaa True/False oikein

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -13,3 +13,11 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_kortin_str_metodi_on_toimiva(self):
         self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_ota_rahaa_palauttaa_true_jos_kortilla_on_katetta(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(200), True)
+
+    def test_ota_rahaa_palauttaa_false_jos_kortilla_ei_ole_katetta(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(2000), False)
+
+    
