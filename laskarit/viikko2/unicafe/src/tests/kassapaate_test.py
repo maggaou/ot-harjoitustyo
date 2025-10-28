12a703dac4a57bb02de2a795ace1432c90c88571
Oula Magga <oula.magga@helsinki.fi>
2025-10-28 15:36:30 +0200

    Viikko 2, tehtävä 8. Testit/käteisosto/lounaiden määrän kasvu 2

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -25,3 +25,11 @@ class TestKassapaate(unittest.TestCase):
     def test_syo_maukkaasti_kateisella_jos_maksu_on_riittava_niin_lounaiden_maara_kasvaa(self):
         self.kassapaate.syo_maukkaasti_kateisella(400)
         self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_syo_edullisesti_kateisella_jos_maksu_ei_ole_riittava_niin_lounaiden_maara_ei_muutu(self):
+        self.kassapaate.syo_edullisesti_kateisella(200)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_syo_maukkaasti_kateisella_jos_maksu_ei_ole_rittava_niin_lounaiden_maara_ei_muutu(self):
+        self.kassapaate.syo_maukkaasti_kateisella(200)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
