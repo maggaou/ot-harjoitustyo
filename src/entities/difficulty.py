3358c6e8b37f487f30573cfc4c8fddb500dab79e
Oula Magga <oula.magga@helsinki.fi>
2025-11-06 17:55:47 +0200

    lisätty Difficulty

---
+++ src/entities/difficulty.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+class Difficulty(StrEnum):
+    BASIC = "basic"
+    INTERMEDIATE = "intermediate"
+    ADVANCED = "advanced"
--- src/tests/moves_repository_test.py
+++ src/tests/moves_repository_test.py
@@ -1,5 +1,6 @@
 import unittest
 from entities.age_group import AgeGroup
+from entities.difficulty import Difficulty
 from entities.style import Style
 
 class TestMovesRepository(unittest.TestCase):
@@ -8,3 +9,6 @@ class TestMovesRepository(unittest.TestCase):
 
     def test_age_group_as_string(self):
         self.assertEqual(AgeGroup.SIX, "6 years old and above")
+    
+    def test_difficulty_as_string(self):
+        self.assertEqual(Difficulty.BASIC, "basic")
