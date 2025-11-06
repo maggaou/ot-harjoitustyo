f712da0c1b1c0c3678dd40e7ef1a52f492dae14c
Oula Magga <oula.magga@helsinki.fi>
2025-11-06 10:22:20 +0200

    Lisää testejä, erityisesti UserRepository.delete_all()

--- src/tests/user_repository_test.py
+++ src/tests/user_repository_test.py
@@ -57,9 +57,26 @@ class TestUserRepository(unittest.TestCase):
         self.assertEqual(users[0],self.u1)
         self.assertEqual(users[1],self.u2)
 
-    def test_find_by_username(self):
+    def test_find_by_username_returns_user_if_found(self):
         self.user_repository.create(self.u1)
 
         u = self.user_repository.find_by_username(self.u1.username)
 
         self.assertEqual(u, self.u1)
+
+    def test_find_by_username_returns_none_if_not_found(self):
+        self.user_repository.create(self.u1)
+
+        u = self.user_repository.find_by_username("jeihou")
+
+        self.assertEqual(u, None)
+    
+    def test_find_all_returns_empty_list_after_delete_all(self):
+        self.user_repository.create(self.u1)
+        self.user_repository.create(self.u2)
+
+        self.user_repository.delete_all()
+
+        hi = self.user_repository.find_all()
+        self.assertEqual(type(hi), list)
+        self.assertEqual(len(hi), 0)
